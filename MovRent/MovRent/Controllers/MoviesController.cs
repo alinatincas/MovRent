e10c5d7aef32ad2a7fc1948afb772e124bfeecbe
Alina Tincas <alin0831@edu.eal.dk>
2018-06-22 23:50:01 +0200

    Created Controller: MoviesController

---
+++ MovRent/MovRent/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovRent.Models;
+
+namespace MovRent.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            // Create an instance of our MovieModel
+            var movie = new Movie() { Name = "Cinderella"}
+            return View();
+        }
+    }
+}
