d15fc0004dfceff08daf0e93aaa0fdc34cbf2c8c
Alina Tincas <alin0831@edu.eal.dk>
2018-06-23 00:50:33 +0200

    Change styling- added another bootstrap.css
    + changed new css to bundle config for fast page load

--- MovRent/MovRent/App_Start/BundleConfig.cs
+++ MovRent/MovRent/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MovRent
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap2.css",
                       "~/Content/site.css"));
         }
     }
