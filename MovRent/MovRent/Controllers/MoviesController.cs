dd4d845e1f39ff8c34dbb0b9e75a50d886fca2fc
Alina Tincas <alin0831@edu.eal.dk>
2018-06-24 10:31:17 +0200

    Tried parameter sources

--- MovRent/MovRent/Controllers/MoviesController.cs
+++ MovRent/MovRent/Controllers/MoviesController.cs
@@ -16,5 +16,24 @@ namespace MovRent.Controllers
             var movie = new Movie() { Name = "Cinderella" };
             return View(movie);
         }
+
+        // MVC maps requests data to parmeters our actions
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // optional param in pur actions
+        // action called when we navigate to movies
+        // return view with list of movies in the DB
+        public ActionResult Index(int? pageIndex, string sortBy) 
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
